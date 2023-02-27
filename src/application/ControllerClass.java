cd3aaa0e3293baa27401c52aaf90c257abe5fe1e
Oolq <96782242+Oolq@users.noreply.github.com>
2023-02-27 14:58:02 +0800

    added fxml file and Controller class'
    '

---
+++ src/application/ControllerClass.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class ControllerClass {
+
+    @FXML
+    private ImageView computerImg;
+
+    @FXML
+    private Button paperBtn;
+
+    @FXML
+    private ImageView playerImg;
+
+    @FXML
+    private Button rockBtn;
+
+    @FXML
+    private Button scissorBtn;
+
+}
+
